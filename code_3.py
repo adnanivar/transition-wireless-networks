e7468daaeee0465c1b1ca7bf13e0f56b0563f00b
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2023-11-08 23:02:40 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,69 @@
+    data = generate_random_data()
+if __name__ == "__main__":
+    return data
+
+        print(f"Random Number: {item}")
+    main()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+
+
+def generate_random_data():
+def main():if __name__ == "__main__":
+
+
+    return data
+
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+def generate_random_data():
+def main():
+import random
+    main()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    main()
+
+        print(f"Random Number: {item}")
+import random
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    data = generate_random_data()
+
+def generate_random_data():
+
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    data = generate_random_data()
+import random
+
+def main():
+    for item in data:
+    main()
+
+def generate_random_data():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")}
+    for (let i = 0; i < 10; i++) {
+main();
+    const data = generateRandomData();
+    return data;
+function generateRandomData() {
+}
+
+
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+    });
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+
+    }
