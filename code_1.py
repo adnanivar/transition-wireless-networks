42c408e5f4d8aebaf4ee140f87d3a2a43859c936
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2023-11-08 15:31:12 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,67 @@
+
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+}
+
+    }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+        }
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        }
+
+import random
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
+if __name__ == "__main__":
+    main()
+        print(f"Random Number: {item}")
+def main():
+    for item in data:
+def generate_random_data():
+    const data = [];
+    data.forEach(item => {
+
+    }
+}
+    return data;
+        console.log("Random Number: " + item);
+    });
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+    for (let i = 0; i < 10; i++) {
+
+}
+
+main();
+function generateRandomData() {
+
+def main():
+def generate_random_data():
+    for item in data:
+    return data
+    main()
+import random
+
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+        print(f"Random Number: {item}")            System.out.println("Random Number: " + item);
+        }
+    public static void main(String[] args) {
+
+        int[] data = new int[10];
+
+        }
+        for (int i = 0; i < 10; i++) {
+    }
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+public class RandomDataGenerator {
